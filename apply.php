35866ed63aa45be766fa7b3b0636442a1d20d9c0
Alex Hugon <ahugon@post.harvard.edu>
2012-08-28 09:10:34 -0400

    dropped in analytics for index & apply

--- apply.php
+++ apply.php
@@ -12,6 +12,25 @@
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 	<script src="js/jquery-1.7.1.min.js"></script>
 	<script src="js/custom.js"></script>
+	
+	<script type="text/javascript">
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-29629736-1']);
+	  _gaq.push(['_setDomainName', 'bostonstartupschool.com']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+		var ga = document.createElement('script'); ga.type =
+	'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' :
+	'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>
+	
 </head>
 
 <body>
